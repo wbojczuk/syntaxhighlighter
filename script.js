8c4b3fcafe23a6450d631fc9a7939bdf5f67ac07
William Bojczuk <williambojczuk@gmail.com>
2022-04-22 15:07:01 -0400

    HTML link underlining

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("testDiv").textContent ='<!DOCTYPE html><br>\
         <meta charset="UTF-8"><br>\
         <meta http-equiv="X-UA-Compatible" content="IE=edge"><br>\
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/><br>\
-        <title>Code Formatting Testing</title><br>\
+        <title>Syntax Highlighting Demo</title><br>\
         <link rel="stylesheet" href="style.css"><br>\
         <!-- Yes... I made this --><br>\
         <script src="script.js" defer></script><br>\
