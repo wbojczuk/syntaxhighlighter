34b6e897795192be2ba8481e09e222609747e9a8
William Bojczuk <williambojczuk@gmail.com>
2022-04-22 12:55:31 -0400

    HTML link underlining

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ document.getElementById("testDiv").textContent ='<!DOCTYPE html><br>\
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/><br>\
         <title>Code Formatting Testing</title><br>\
         <link rel="stylesheet" href="style.css"><br>\
-        <!-- This is a comment <element> --><br>\
+        <!-- Yes... I made this --><br>\
         <script src="script.js" defer></script><br>\
-        <script src="codeformatter.js" defer></script>&#10;<br>\
-        <a href="https://google.com" target="_blank">A LINK</a><br>\
+        <script src="syntaxhighlighter.js" defer></script><br>\
+        <a href="https://williambojczuk.com" target="_blank">Myself</a><br>\
     </head>';
 
     
