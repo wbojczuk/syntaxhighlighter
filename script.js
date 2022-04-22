0dec4ce759c83a66590cb551e25fd374f2f4ec4a
William Bojczuk <williambojczuk@gmail.com>
2022-04-22 11:46:20 -0400

    HTML link underlining

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
 document.getElementById("testDiv").textContent ='<!DOCTYPE html><br>\
-<html lang="en"><br>\
-<head><br>\
+<html lang="en-us"><br>\
+    <head><br>\
         <meta charset="UTF-8"><br>\
         <meta http-equiv="X-UA-Compatible" content="IE=edge"><br>\
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/><br>\
